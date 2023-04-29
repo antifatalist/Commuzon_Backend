81cdb4da77d3e28c785dd49b94a7e30119eebddd
antifatalist <antifatalism@proton.me>
2023-04-29 15:35:03 -0400

    Add email for user

--- models/User.js
+++ models/User.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
   // id field automatically added on post
 
+  email: {
+    type: String,
+    required: false,
+  },
   username: {
     type: String,
     required: true,
